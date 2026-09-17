e1eefaee2abd0c6369478c1c8e089477fbc72967
tucuninho <goncalves.pereira.jose@escola.pr.gov.br>
2026-09-17 09:41:30 -0300

    Add files via upload

---
+++ atividade 3/script.js
@@ -0,0 +1,11 @@
+let nota1tri;
+let nota2tri;
+let resultado;
+
+function calcular(){
+    nota1tri = Number(prompt("diga a nota do primeiro trimestre:"));
+     nota2tri = Number(prompt("diga a nota do segundo trimestre:"));
+
+     resultado = 180 - nota1tri + nota2tri
+     
+}
